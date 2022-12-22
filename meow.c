bcfb889aa07d3894886b10cb49927eeff254550b
BernardoDeveloper <bernardogualberto8@gmail.com>
2022-12-22 11:54:21 -0300

    feat: (meow) loop and functions

--- meow.c
+++ meow.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
+// define the function
+void meow(int n);
+
 int main(void)
 {
+  printf("While: \n");
   int counter = 0;
   while (counter < 3)
   {
@@ -9,11 +13,21 @@ int main(void)
     counter++;
   }
 
-  printf("---\n");
-
   // or can you make a loop in this form:
+  printf("\nFor: \n");
   for (int i = 0; i < 3; i++)
   {
     printf("meow\n");
   }
+
+  // make a function to do this;
+  printf("\nFunction: \n");
+  meow(3);
+}
+
+// create the function
+void meow(int n)
+{
+  for (int i = 0; i < 3; i++)
+    printf("meow\n");
 }
