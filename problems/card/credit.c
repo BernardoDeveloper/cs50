8eafe548c136abc9e8ea8690003156adab3b5de8
BernardoDeveloper <bernardogualberto8@gmail.com>
2022-12-28 10:21:27 -0300

    feat: verify if card number is true

--- problems/card/credit.c
+++ problems/card/credit.c
@@ -1,17 +1,45 @@
+#include <cs50.h>
 #include <stdio.h>
+#include <string.h>
+
+bool check_card_number(char cardNumber[1000]);
+int str_length(char str[]);
 
 int main(void)
 {
-    long number, sum = 0;
-    printf("Type card number: ");
-    scanf("%li", &number);
+	char number[1000];
+	printf("Type card number: ");
+	scanf("%s", number);
 
-    while (number != 0)
-    {
-        sum += number % 10;
-        number -= number % 10;
-        number = (int)(number / 10);
-    }
+	bool isValid = check_card_number(number);
+	if (isValid == 0)
+		printf("Invalid credit card.\n");
+}
 
-    printf("%li %li", number, sum);
+bool check_card_number(char cardNumber[1000])
+{
+	char numberDigits = str_length(cardNumber);
+
+	int numberSum = 0, isSecond = false;
+	for (int i = numberDigits - 1; i >= 0; i--)
+	{
+		int d = cardNumber[i] - '0';
+		if (isSecond == true)
+			d *= 2;
+
+		numberSum += d / 10;
+		numberSum += d % 10;
+
+		isSecond = !isSecond;
+	}
+
+	return (numberSum % 10 == 0);
+}
+
+int str_length(char str[])
+{
+	int count;
+	for (count = 0; str[count] != '\0'; count++)
+		;
+	return count;
 }
