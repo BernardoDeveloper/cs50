042cf5c629b220e4e13d87770b1327d5dab27015
BernardoDeveloper <bernardogualberto8@gmail.com>
2022-12-30 11:12:18 -0300

    fix: bugs and brand validation

--- problems/card/credit.c
+++ problems/card/credit.c
@@ -4,6 +4,7 @@
 
 bool check_card_number(char cardNumber[1000]);
 int str_length(char str[]);
+void verify_brand_card(char number[]);
 
 int main(void)
 {
@@ -12,8 +13,13 @@ int main(void)
 	scanf("%s", number);
 
 	bool isValid = check_card_number(number);
-	if (isValid == 0)
+	if (isValid == false)
+	{
 		printf("Invalid credit card.\n");
+		return 1;
+	}
+
+	verify_brand_card(number);
 }
 
 bool check_card_number(char cardNumber[1000])
@@ -39,7 +45,14 @@ bool check_card_number(char cardNumber[1000])
 int str_length(char str[])
 {
 	int count;
-	for (count = 0; str[count] != '\0'; count++)
-		;
+	for (count = 0; str[count] != '\0'; count++);
 	return count;
+}
+
+void verify_brand_card(char number[])
+{
+	if (number[0] == '4')
+		printf("VISA\n");
+	else if (number[0] == '5' && number[1] == '1' || number[0] == '5' && number[1] == '5')
+		printf("MASTER\n");
 }
