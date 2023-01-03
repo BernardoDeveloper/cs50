419fd5b70cb014ae64a176728ff034b209afd5c2
BernardoDeveloper <bernardogualberto8@gmail.com>
2023-01-03 11:11:27 -0300

    fix: values and loops

--- problems/card/two_points.c
+++ problems/card/two_points.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(void)
 {
-    float axle[4];
-    for (int i = 0; i >= 4; i++)
-        scanf("%f", &axle[i]);
-        printf("%f", &axle[i]);
+    printf("X:\n");
+    float axleX[2];
+    for (int i = 0; i < 2; i++)
+        scanf("%f", &axleX[i]);
+    
+    printf("Y:\n");
+    float axleY[2];
+    for (int i = 0; i < 2; i++)
+        scanf("%f", &axleY[i]);
+
+    float dx = axleX[0] - axleX[1];
+    float dy = axleY[0] - axleY[1];
+
+    float distance = sqrt(dx * dx + dy * dy);
+    printf("RESULT: %.4f", distance);
 }
